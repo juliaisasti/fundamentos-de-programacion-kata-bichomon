77b210f79ad2fce5a67c01a86dbed6fdadc897e4
Julia <juliaisasti@gmail.com>
2023-10-03 14:22:02 +0200

    final

--- scripts.js
+++ scripts.js
@@ -10,3 +10,8 @@ console.log(document.domain)
 
 const images = document.querySelectorAll("img")
 
+console.log(images);
+
+for (let i = 0; i < images.length; i++) {
+   images[i].src="./giphy.gif"
+}
