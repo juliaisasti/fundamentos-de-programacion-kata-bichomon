e05e371876161f2b39e8dd700104ddc62543dfde
Julia <juliaisasti@gmail.com>
2023-10-03 09:41:23 +0200

    x

--- scripts.js
+++ scripts.js
@@ -1,1 +1,12 @@
 console.log(document.title);
+
+document.getElementById("infocardBulbasaur").style.backgroundColor = "red";
+
+let currentLocation = window.location;
+
+console.log(currentLocation)
+
+console.log(document.domain)
+
+const images = document.querySelectorAll("img")
+
